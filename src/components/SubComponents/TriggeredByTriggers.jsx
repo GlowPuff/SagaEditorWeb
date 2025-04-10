473880203fa64b1989f4da6a3e055ecbae11622a
GlowPuff <glowpuffmobile@gmail.com>
2025-04-10 04:02:37 -0400

    cleaned up UI for TriggeredByTriggers

--- src/components/SubComponents/TriggeredByTriggers.jsx
+++ src/components/SubComponents/TriggeredByTriggers.jsx
@@ -17,6 +17,7 @@ import {
 } from "../../data/carddata";
 import { useToonsStore } from "../../data/dataStore";
 import { CharacterType } from "../../lib/core";
+import { Paper } from "@mui/material";
 
 export default function TriggeredByTriggers({ missionEvent, modifyEvent }) {
   const customHeroes = useToonsStore((state) =>
@@ -84,227 +85,243 @@ export default function TriggeredByTriggers({ missionEvent, modifyEvent }) {
   }
 
   return (
-    <div className="mission-panel">
-      <div className="label-text">
-        <FormControlLabel
-          control={
-            <Checkbox
-              name="useStartOfRound"
-              checked={missionEvent.useStartOfRound}
-              onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+    <div>
+      <div className="two-column-grid">
+        {/* LEFT */}
+        <Paper sx={{ padding: "1rem" }}>
+          {/* START OF ROUND */}
+          <div className="label-text">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="useStartOfRound"
+                  checked={missionEvent.useStartOfRound}
+                  onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+                />
+              }
+              label="Start of Round:"
             />
-          }
-          label="Start of Round"
-        />
+            <TextField
+              type="number"
+              name="startOfRound"
+              variant="filled"
+              value={missionEvent.startOfRound}
+              onChange={(e) => modifyEvent(e.target.name, e.target.value)}
+              onFocus={(e) => e.target.select()}
+              onKeyUp={onKeyUp}
+              sx={{ marginBottom: "1rem" }}
+            />
+          </div>
+          {/* END OF ROUND */}
+          <div className="label-text">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  name="useEndOfRound"
+                  checked={missionEvent.useEndOfRound}
+                  onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+                />
+              }
+              label="End of Round:"
+            />
+            <TextField
+              type="number"
+              name="endOfRound"
+              variant="filled"
+              value={missionEvent.endOfRound}
+              onChange={(e) => modifyEvent(e.target.name, e.target.value)}
+              onFocus={(e) => e.target.select()}
+              onKeyUp={onKeyUp}
+              sx={{ marginBottom: "1rem" }}
+            />
+          </div>
+          {/* START OF EACH ROUND */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useStartOfEachRound"
+                checked={missionEvent.useStartOfEachRound}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Start of Each Round"
+          />
+          {/* END OF EACH ROUND */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useEndOfEachRound"
+                checked={missionEvent.useEndOfEachRound}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="End of Each Round"
+          />{" "}
+        </Paper>
 
-        <TextField
-          type="number"
-          label="Start of Round"
-          name="startOfRound"
-          variant="filled"
-          value={missionEvent.startOfRound}
-          onChange={(e) => modifyEvent(e.target.name, e.target.value)}
-          onFocus={(e) => e.target.select()}
-          fullWidth
-          onKeyUp={onKeyUp}
-          sx={{ marginBottom: "1rem" }}
-        />
+        {/* RIGHT */}
+        <Paper sx={{ padding: "1rem" }}>
+          {/* ALL HEROES WOUNDED */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useAllHeroesWounded"
+                checked={missionEvent.useAllHeroesWounded}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="All Heroes Are Wounded"
+          />
+          {/* ANY HERO WOUNDED */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useAnyHeroWounded"
+                checked={missionEvent.useAnyHeroWounded}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Any Hero Wounded"
+          />
+
+          {/* ANY HERO WITHDRAWS */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useAnyHeroDefeated"
+                checked={missionEvent.useAnyHeroDefeated}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Any Hero Withdraws"
+          />
+					<hr/>
+          {/* ALL GROUPS DEFEATED */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useAllGroupsDefeated"
+                checked={missionEvent.useAllGroupsDefeated}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="All Enemy Groups Defeated"
+          />
+        </Paper>
       </div>
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useStartOfEachRound"
-            checked={missionEvent.useStartOfEachRound}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+
+      <Paper sx={{ padding: "1rem", marginTop: ".5rem" }}>
+        <div className="mission-panel">
+          {/* ALLY DEFEATED */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useAllyDefeated"
+                checked={missionEvent.useAllyDefeated}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Ally Defeated"
           />
-        }
-        label="Start of Each Round"
-      />
-      {/* === */}
-      <div className="label-text">
-        <FormControlLabel
-          control={
-            <Checkbox
-              name="useEndOfRound"
-              checked={missionEvent.useEndOfRound}
-              onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-            />
-          }
-          label="End of Round"
-        />
-        <TextField
-          type="number"
-          label="End of Round"
-          name="endOfRound"
-          variant="filled"
-          value={missionEvent.endOfRound}
-          onChange={(e) => modifyEvent(e.target.name, e.target.value)}
-          onFocus={(e) => e.target.select()}
-          fullWidth
-          onKeyUp={onKeyUp}
-          sx={{ marginBottom: "1rem" }}
-        />
-      </div>
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useEndOfEachRound"
-            checked={missionEvent.useEndOfEachRound}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+          <FormControl>
+            <InputLabel>Ally Defeated</InputLabel>
+            <Select
+              name="allyDefeated"
+              value={allyDefeated}
+              onChange={(e) => changeAllyDefeated(e.target.value)}
+              displayEmpty
+              disabled={!missionEvent.useAllyDefeated}
+            >
+              {allyArray.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* HERO WOUNDED */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useHeroWounded"
+                checked={missionEvent.useHeroWounded}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Hero Wounded"
           />
-        }
-        label="End of Each Round"
-      />
-      {/* === */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useAllGroupsDefeated"
-            checked={missionEvent.useAllGroupsDefeated}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+          <FormControl>
+            <InputLabel>Hero Wounded</InputLabel>
+            <Select
+              name="heroWounded"
+              value={heroWounded}
+              onChange={(e) => changeHeroWounded(e.target.value)}
+              displayEmpty
+              disabled={!missionEvent.useHeroWounded}
+            >
+              {heroArray.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* HERO WITHDRAWS */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useHeroWithdraws"
+                checked={missionEvent.useHeroWithdraws}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Hero Withdraws"
           />
-        }
-        label="All Enemy Groups Defeated"
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useAllHeroesWounded"
-            checked={missionEvent.useAllHeroesWounded}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+          <FormControl>
+            <InputLabel>Hero Withdraws</InputLabel>
+            <Select
+              name="heroWithdraws"
+              value={heroWithdraws}
+              onChange={(e) => changeHeroWithdraws(e.target.value)}
+              displayEmpty
+              disabled={!missionEvent.useHeroWithdraws}
+            >
+              {heroArray.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  {item.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {/* ACTIVATION OF */}
+          <FormControlLabel
+            control={
+              <Checkbox
+                name="useActivation"
+                checked={missionEvent.useActivation}
+                onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
+              />
+            }
+            label="Activation Of"
           />
-        }
-        label="All Heroes Are Wounded"
-      />
-      {/* === */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useAnyHeroDefeated"
-            checked={missionEvent.useAnyHeroDefeated}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-          />
-        }
-        label="Any Hero Withdraws"
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useAnyHeroWounded"
-            checked={missionEvent.useAnyHeroWounded}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-          />
-        }
-        label="Any Hero Wounded"
-      />
-      {/* === */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useAllyDefeated"
-            checked={missionEvent.useAllyDefeated}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-          />
-        }
-        label="Ally Defeated"
-      />
-      <FormControl>
-        <InputLabel>Ally Defeated</InputLabel>
-        <Select
-          name="allyDefeated"
-          value={allyDefeated}
-          onChange={(e) => changeAllyDefeated(e.target.value)}
-          displayEmpty
-          disabled={!missionEvent.useAllyDefeated}
-        >
-          {allyArray.map((item, index) => (
-            <MenuItem key={index} value={item}>
-              {item.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {/* === */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useHeroWounded"
-            checked={missionEvent.useHeroWounded}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-          />
-        }
-        label="Hero Wounded"
-      />
-      <FormControl>
-        <InputLabel>Hero Wounded</InputLabel>
-        <Select
-          name="heroWounded"
-          value={heroWounded}
-          onChange={(e) => changeHeroWounded(e.target.value)}
-          displayEmpty
-          disabled={!missionEvent.useHeroWounded}
-        >
-          {heroArray.map((item, index) => (
-            <MenuItem key={index} value={item}>
-              {item.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {/* === */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useHeroWithdraws"
-            checked={missionEvent.useHeroWithdraws}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-          />
-        }
-        label="Hero Withdraws"
-      />
-      <FormControl>
-        <InputLabel>Hero Withdraws</InputLabel>
-        <Select
-          name="heroWithdraws"
-          value={heroWithdraws}
-          onChange={(e) => changeHeroWithdraws(e.target.value)}
-          displayEmpty
-          disabled={!missionEvent.useHeroWithdraws}
-        >
-          {heroArray.map((item, index) => (
-            <MenuItem key={index} value={item}>
-              {item.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      {/* === */}
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="useActivation"
-            checked={missionEvent.useActivation}
-            onChange={(e) => modifyEvent(e.target.name, e.target.checked)}
-          />
-        }
-        label="Activation Of"
-      />
-      <FormControl>
-        <InputLabel>Enemy Activates</InputLabel>
-        <Select
-          name="activationOf"
-          value={enemyActivate}
-          onChange={(e) => changeEnemyActivate(e.target.value)}
-          displayEmpty
-          disabled={!missionEvent.useActivation}
-        >
-          {enemyArray.map((item, index) => (
-            <MenuItem key={index} value={item}>
-              {item.name}: {item.id}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+          <FormControl>
+            <InputLabel>Enemy Activates</InputLabel>
+            <Select
+              name="activationOf"
+              value={enemyActivate}
+              onChange={(e) => changeEnemyActivate(e.target.value)}
+              displayEmpty
+              disabled={!missionEvent.useActivation}
+            >
+              {enemyArray.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  {item.name}: {item.id}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+      </Paper>
     </div>
   );
 }
